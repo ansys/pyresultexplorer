79514d6fdc8732a905b2edcbd67fe3bbe2b892ff
Federico Negri <federico.negri@ansys.com>
2025-12-09 15:31:52 +0100

    update package, test import

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -16,3 +16,7 @@ def test_grpc_import():
         import grpc  # noqa: F401
     except ImportError as e:
         raise ImportError("The grpc module is not installed.") from e
+
+
+def test_api_import():
+    from ansys.api.result_explorer.v0.workspace_pb2_grpc import WorkspaceServiceStub  # noqa
