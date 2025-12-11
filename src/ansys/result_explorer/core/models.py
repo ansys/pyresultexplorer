ab52fd21c3574eaf694e8493c2ebdd2389bdc2af
Federico Negri <federico.negri@ansys.com>
2025-12-11 14:35:02 +0100

    additional apis

--- src/ansys/result_explorer/core/models.py
+++ src/ansys/result_explorer/core/models.py
@@ -1,3 +1,14 @@
-from ansys.api.result_explorer.v0.base_pb2 import Empty  # noqa
-from ansys.api.result_explorer.v0.solution_pb2 import SolutionCreate, AppSolution, AppSolutionList  # noqa
-from ansys.api.result_explorer.v0.workspace_pb2 import WorkspaceCreate, Workspace, WorkspaceList  # noqa
+from ansys.api.result_explorer.v0.base_pb2 import Empty, ResourceId  # noqa
+from ansys.api.result_explorer.v0.solution_pb2 import (
+    SolutionCreate,
+    AppSolution,
+    AppSolutionList,
+    ViewList,
+)  # noqa
+from ansys.api.result_explorer.v0.workspace_pb2 import (
+    WorkspaceCreate,
+    Workspace,
+    WorkspaceList,
+    AssignViewRequest,
+)  # noqa
+from ansys.api.result_explorer.v0.snapshot_pb2 import SnapshotRequest, SnapshotSettings, Snapshot  # noqa
