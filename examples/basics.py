19df66d0bd8811e60b75934657d807865fb406c2
Federico Negri <federico.negri@ansys.com>
2025-12-11 17:33:04 +0100

    update example

--- examples/basics.py
+++ examples/basics.py
@@ -1,3 +1,8 @@
+import os
+import time
+
+import matplotlib.image as img
+import matplotlib.pyplot as plt
 from slugify import slugify
 
 from ansys.result_explorer.core.client import Client
@@ -16,7 +21,7 @@ sol_name = "PyRX Solution"
 sol = rx.create_solution(
     result_provider_name="Local",
     name=sol_name,
-    file_path=r"D:\Models\mech-post\beam_2_mat.rst",
+    file_path=r"D:\Models\mech-post\cylinder_plate\d3plot",
 )
 
 views = rx.get_views(solution_id=sol.id)
@@ -25,9 +30,23 @@ print(views)
 view = next((v for v in views.views if "Min/Max" in v.name), None)
 assert view is not None
 
+print(f"Opening view: {view.name} in the workspace.")
 portal_id = rx.assign_view(workspace_id=workspace.id, view_id=view.id)
 
+print("Waiting for the view to load...")
+time.sleep(1)  # review in the web repo
+
+print("Taking snapshot...")
 snapshot_data = rx.take_snapshot(portal_id=portal_id)
 
-with open(slugify(sol_name + " - " + view.name) + ".png", "wb") as image_file:
+print("Saving snapshot to file...")
+file_name = slugify(sol_name + " - " + view.name) + ".png"
+with open(file_name, "wb") as image_file:
     image_file.write(snapshot_data)
+
+print(f"Snapshot saved to: {os.path.absfile_name}")
+
+print("Displaying snapshot...")
+im = img.imread(file_name)
+plt.imshow(im)
+plt.show()
