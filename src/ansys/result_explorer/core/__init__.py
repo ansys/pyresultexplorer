324d8f1ff64d7bad53055cc2500fcf5e8a881f7b
Federico Negri <FedericoNegri@users.noreply.github.com>
2026-01-27 13:33:58 +0100

    Test error handling and client side session ID (#18)

--- src/ansys/result_explorer/core/__init__.py
+++ src/ansys/result_explorer/core/__init__.py
@@ -5,4 +5,5 @@ import importlib.metadata as importlib_metadata
 __version__ = importlib_metadata.version(__name__.replace(".", "-"))
 
 from .client import Client
+from .exceptions import ResultExplorerError
 from .logger import log
